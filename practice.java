6b0cf73b7e2e634ae9c120071a89069041d565bb
Mohanaprasath31 <itzprasath31@gmail.com>
2025-03-07 23:30:40 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -49,3 +49,63 @@ class Cat extends Animal{
       }
     
 ------------------------------------------------------------------------------------------------------------------------------------------------
+class Book {
+    private String name;
+    private String author;
+    private String isbn;
+    private int stock;
+    public Book() {
+        this.name = "Unknown";
+        this.author = "Unknown";
+        this.isbn = "000000";
+        this.stock = 0;
+    }
+    public Book(String name, String author, String isbn, int stock) {
+        this.name = name;
+        this.author = author;
+        this.isbn = isbn;
+        this.stock = stock;
+    }
+    public Book(Book other) {
+        this.name = other.name;
+        this.author = other.author;
+        this.isbn = other.isbn;
+        this.stock = other.stock;
+    }
+    public void displayDetails() {
+        System.out.println("Book Name: " + name);
+        System.out.println("Author: " + author);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Stock: " + stock);
+        System.out.println("-----------------------");
+    }
+
+    public void updateStock(int newStock) {
+        if (newStock >= 0) {
+            this.stock = newStock;
+            System.out.println("Stock updated successfully.");
+        } else {
+            System.out.println("Stock cannot be negative.");
+        }
+    }
+    public class Library {
+    public static void main(String args[]) {
+        Book book1 = new Book();
+        System.out.println("Default Constructor:");
+        book1.displayDetails();
+
+        Book book2 = new Book("The Alchemist", "Paulo Coelho", "123456789", 10);
+        System.out.println("Parameterized Constructor:");
+        book2.displayDetails();
+        
+        Book book3 = new Book(book2);
+        System.out.println("Copy Constructor (Duplicate Book):");
+        book3.displayDetails();
+        System.out.println("Updating stock for 'The Alchemist'...");
+        book2.updateStock(15);
+        book2.displayDetails();
+    }
+  }
+}
+
+
